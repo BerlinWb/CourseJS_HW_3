8a2b945745ead05a4247e3d2348cbf90031b42f0
Oleh Svetlov <95508050+BerlinWb@users.noreply.github.com>
2022-07-04 00:52:28 +0300

    Update index.js

--- Course_Js_Task3_v2.0/js/index.js
+++ Course_Js_Task3_v2.0/js/index.js
@@ -1,35 +1,26 @@
-"use strict";
+"use stric";
 
 
-function calcAdd() {
     const a = parseFloat(prompt("first"));
     const b = parseFloat(prompt("Second"));
     let result;
+function calcAdd() {
     result = a + b;
     console.log(`Result: ${a} + ${b} = ${result}`);
 }
 function calcMin() {
-    const a = parseFloat(prompt("first"));
-    const b = parseFloat(prompt("Second"));
-    let result;
     result = a - b;
     console.log(`Result: ${a} - ${b} = ${result}`);
 }
 function calcMult() {
-    const a = parseFloat(prompt("first"));
-    const b = parseFloat(prompt("Second"));
-    let result;
     result = a / b;
     console.log(`Result: ${a} / ${b} = ${result}`);
 }
 function calcDiv() {
-    const a = parseFloat(prompt("first"));
-    const b = parseFloat(prompt("Second"));
-    let result;
     result = a * b;
     console.log(`Result: ${a} * ${b} = ${result}`);
 }
 calcAdd();
 calcMin();
 calcMult();
-calcDiv(); 
+calcDiv();
